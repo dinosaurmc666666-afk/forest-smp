b81c99b18f864bc40c3b59c94a38a697432fe39d
P0KEM0N <dinosaurmc666666@gmail.com>
2026-06-29 20:17:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function getProducts() {
     }
 }
 
-const API_BASE_URL = "http://127.0.0.1:55071";
+const API_BASE_URL = "https://backend-11zq.onrender.com";
 
 let currentProduct = null;
 let countdownInterval = null;
